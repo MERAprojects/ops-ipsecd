82f6bd4f4f2a2e89aa9626e80cc396f91e7a9e2e
Carlos Chacon <carlos.chacon@hpe.com>
2016-05-05 18:29:29 -0700

    new: dev: adding unit tests for ErrorNotifySS class part 2
    Change-Id: If05fb86afef69daacb5849453e6d37524189763e
    Signed-off-by: Carlos Chacon <carlos.chacon@hpe.com>

--- tests/error_notify_ss_tests.cpp
+++ tests/error_notify_ss_tests.cpp
@@ -211,3 +211,192 @@ TEST_F(ErrorNotifySSTestSuite, TestCleanUp)
     EXPECT_FALSE(m_err_notify.get_is_running());
     EXPECT_EQ(m_err_notify.get_conn(), 0);
 }
+
+/**
+ * Objective: Verify that clean up will stop the thread and close the socket if
+ * the socket was created
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestCleanUpSocketNotOpen)
+{
+    int conn = 0;
+
+    /////////////////////////////////////////////////
+
+    EXPECT_FALSE(m_err_notify.get_is_ready());
+    EXPECT_FALSE(m_err_notify.get_is_running());
+    EXPECT_EQ(m_err_notify.get_conn(), 0);
+
+    m_err_notify.set_is_ready(true);
+    m_err_notify.set_is_running(true);
+    m_err_notify.set_conn(conn);
+
+    EXPECT_TRUE(m_err_notify.get_is_ready());
+    EXPECT_TRUE(m_err_notify.get_is_running());
+    EXPECT_EQ(m_err_notify.get_conn(), conn);
+
+    /////////////////////////////////////////////////
+
+    EXPECT_CALL(m_system_calls, s_close(_))
+            .Times(0);
+
+    /////////////////////////////////////////////////
+
+    m_err_notify.call_cleanup();
+
+    /////////////////////////////////////////////////
+
+    EXPECT_FALSE(m_err_notify.get_is_ready());
+    EXPECT_FALSE(m_err_notify.get_is_running());
+    EXPECT_EQ(m_err_notify.get_conn(), 0);
+}
+
+/**
+ * Objective: Verify that initialize will start the thread and create the socket
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestInitialize)
+{
+    FakeCalls fakeCalls;
+    int conn = 100;
+    ssize_t len = 0;
+    ssize_t size = sizeof(error_notify_msg_t);
+
+    ////////////////////////////////////////
+
+    fakeCalls.m_error_notify = &m_err_notify;
+
+    len = strlen(ERROR_NOTIFY_SOCKET) + sizeof(sa_family_t);
+
+    ////////////////////////////////////////
+
+    ON_CALL(m_system_calls, s_connect(_, _, _))
+            .WillByDefault(Invoke(&fakeCalls, &FakeCalls::s_connect_init));
+
+    ON_CALL(m_system_calls, s_read(_, _, _))
+            .WillByDefault(Invoke(&fakeCalls, &FakeCalls::s_read_end));
+
+    EXPECT_CALL(m_system_calls, s_socket(Eq(AF_UNIX), Eq(SOCK_STREAM), Eq(0)))
+            .WillOnce(Return(conn));
+
+    EXPECT_CALL(m_system_calls, s_connect(Eq(conn), NotNull(), Eq(len)));
+
+    EXPECT_CALL(m_system_calls, s_read(Eq(conn), NotNull(), Eq(size)));
+
+    ////////////////////////////////////////
+
+    EXPECT_EQ(m_err_notify.initialize(), ipsec_ret::OK);
+
+    ////////////////////////////////////////
+
+    std::thread& err_thread = m_err_notify.get_error_thread();
+
+    err_thread.join();
+}
+
+/**
+ * Objective: Verify that initialize will return the correct error in case of
+ * failure
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestInitializeCreateSocketFailed)
+{
+    int conn = -1;
+
+    ////////////////////////////////////////
+
+    EXPECT_CALL(m_system_calls, s_socket(Eq(AF_UNIX), Eq(SOCK_STREAM), Eq(0)))
+            .WillOnce(Return(conn));
+
+    ////////////////////////////////////////
+
+    EXPECT_EQ(m_err_notify.initialize(), ipsec_ret::SOCKET_CREATE_FAILED);
+}
+
+/**
+ * Objective: Verify that initialize will start the thread and create the socket
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestInitializeConnectSocketFailed)
+{
+    int conn = 100;
+    ssize_t len = 0;
+
+    ////////////////////////////////////////
+
+    len = strlen(ERROR_NOTIFY_SOCKET) + sizeof(sa_family_t);
+
+    ////////////////////////////////////////
+
+    EXPECT_CALL(m_system_calls, s_socket(Eq(AF_UNIX), Eq(SOCK_STREAM), Eq(0)))
+            .WillOnce(Return(conn));
+
+    EXPECT_CALL(m_system_calls, s_connect(Eq(conn), NotNull(), Eq(len)))
+            .WillOnce(Return(-1));
+
+    ////////////////////////////////////////
+
+    EXPECT_EQ(m_err_notify.initialize(), ipsec_ret::SOCKET_CONNECT_FAILED);
+}
+
+/**
+ * Objective: Verify that Error Receiver will be able to process the
+ * errors correctly
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestRunErrorReceiver)
+{
+    int conn = 100;
+    ssize_t size = sizeof(error_notify_msg_t);
+    ssize_t ret = sizeof(error_notify_msg_t) / 2;
+    FakeCalls fakeCalls;
+
+    ////////////////////////////////////////
+
+    fakeCalls.m_error_notify = &m_err_notify;
+
+    m_err_notify.set_conn(conn);
+    m_err_notify.set_is_ready(true);
+    m_err_notify.set_is_running(true);
+
+    ////////////////////////////////////////
+
+    {
+        InSequence s;
+
+        EXPECT_CALL(m_system_calls, s_read(Eq(conn), NotNull(), Eq(size)))
+                .WillOnce(Invoke(&fakeCalls, &FakeCalls::s_read_receive_err_set_msg));
+
+        EXPECT_CALL(m_system_calls, s_read(Eq(conn), NotNull(), Eq(size - ret)))
+                .WillOnce(Return(size - ret));
+
+        EXPECT_CALL(m_system_calls, s_read(Eq(conn), NotNull(), Eq(size)))
+                .WillOnce(Invoke(&fakeCalls, &FakeCalls::s_read_end));
+    }
+
+    ////////////////////////////////////////
+
+    EXPECT_EQ(m_err_notify.call_run_error_receiver(), ipsec_ret::OK);
+}
+
+/**
+ * Objective: Verify that Error Receiver will return the correct error code
+ * in case of failure
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestRunErrorReceiverNotReady)
+{
+    m_err_notify.set_is_ready(false);
+
+    ////////////////////////////////////////
+
+    EXPECT_EQ(m_err_notify.call_run_error_receiver(), ipsec_ret::NOT_READY);
+}
+
+/**
+ * Objective: Verify that Error Receiver will return the correct error code
+ * in case of failure
+ **/
+TEST_F(ErrorNotifySSTestSuite, TestRunErrorReceiverNotRunning)
+{
+    m_err_notify.set_is_ready(true);
+    m_err_notify.set_is_running(false);
+
+    ////////////////////////////////////////
+
+    EXPECT_EQ(m_err_notify.call_run_error_receiver(), ipsec_ret::NOT_RUNNING);
+}
